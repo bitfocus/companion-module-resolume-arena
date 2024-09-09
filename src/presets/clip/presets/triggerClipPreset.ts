f661fe06fa7454beb8eb508e8e9a8e3f49f73267
Axel Bergmans <axel@4xel.be>
2024-09-09 16:57:09 +0200

    chore(): cleanup

--- src/presets/clip/presets/triggerClipPreset.ts
+++ src/presets/clip/presets/triggerClipPreset.ts
@@ -1,6 +1,6 @@
 import {combineRgb} from '@companion-module/base';
 import {CompanionButtonPresetDefinition} from '@companion-module/base/dist/module-api/preset';
-import {getDefaultLayerColumnOptions, getDefaultStyleGreen} from '../../../defaults';
+import {getDefaultLayerColumnOptions} from '../../../defaults';
 
 export function triggerClipPreset(category: string): CompanionButtonPresetDefinition {
 	return {
@@ -27,8 +27,13 @@ export function triggerClipPreset(category: string): CompanionButtonPresetDefini
 		feedbacks: [
 			{
 				feedbackId: 'connectedClip',
-				options: getDefaultLayerColumnOptions(),
-				style: getDefaultStyleGreen()
+				options: {
+					...getDefaultLayerColumnOptions(),
+					color_connected: 'rgb(0, 255, 0)',
+					color_connected_selected: 'rgb(0,255,255)',
+					color_connected_preview: 'rgb(255, 255, 0)',
+					color_preview: 'rgb(255, 0, 0)'
+				}
 			},
 			{
 				feedbackId: 'clipInfo',
